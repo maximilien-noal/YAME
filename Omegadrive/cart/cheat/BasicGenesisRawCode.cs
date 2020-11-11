d2cd8926ea5d8ce8a5c04297af0cf2f5e2559e30
Maximilien Noal <noal.maximilien@gmail.com>
2020-11-11 05:48:35 +0100

    Target .NET 5
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

---
+++ Omegadrive/cart/cheat/BasicGenesisRawCode.cs
@@ -0,0 +1,57 @@
+﻿namespace Omegadrive.cart.cheat
+{
+    public class BasicGenesisRawCode
+    {
+        public static BasicGenesisRawCode INVALID_CODE = new BasicGenesisRawCode(-1, -1);
+        private int address;
+        private int value;
+
+        public BasicGenesisRawCode(int address, int value)
+        {
+            this.SetAddress(address);
+            this.SetValue(value);
+        }
+
+        public virtual void SetAddress(int address)
+        {
+            if ((address & 0xFF000000) == 0)
+            {
+                this.address = address;
+            }
+        }
+
+        public virtual void SetValue(int value)
+        {
+            if ((value & 0xFFFF0000) == 0)
+            {
+                this.value = value;
+            }
+        }
+
+        public virtual int GetAddress()
+        {
+            return this.address;
+        }
+
+        public virtual int GetValue()
+        {
+            return this.value;
+        }
+
+        public virtual string ToHexString(int number, int minLength)
+        {
+            string hex = number.ToString("x").ToUpperInvariant();
+            while (hex.Length < minLength)
+            {
+                hex = "0" + hex;
+            }
+
+            return hex;
+        }
+
+        public override string ToString()
+        {
+            return "GenesisRawCode[" + this.ToHexString(this.GetValue(), 4) + ":" + this.ToHexString(this.GetAddress(), 6) + "]";
+        }
+    }
+}
